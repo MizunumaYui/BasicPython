74ff9514d19dadcca4edbc41a3c1ab9c1215fc04
MizunumaYui <y.mizunuma.24.4.1332@4nonome.com>
2024-04-22 13:36:14 +0900

    制御構文の解答

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,1 +1,6 @@
-# TODO
+epsilon = 1 #イプシロンを1とする
+
+while epsilon + 1 > 1: #最小のイプシロンを求める 
+    epsilon /= 2
+
+print(epsilon)
--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,39 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+#n = int(input("nの値を入力: ")) 
 
-# TODO
+#(1)
+n = 61 
+prime = True #primeを用いてaが素数であると仮定する
+
+if n < 2:
+    print("素数でない")
+
+else: #nが2以上の場合
+    for i in range(2, int(n**(1/2)+1)): #2以上√a以下(整数)で割り切れる値があるか調べる
+        if n % i == 0:
+            prime = False
+            break
+
+    if prime: #True
+        print("素数である")
+
+    else: #False
+        print("素数でない")  
+
+#(2)
+n = 10 
+prime = True #primeを用いてaが素数であると仮定する
+
+if n < 2:
+    print("素数でない")
+
+else: #nが2以上の場合
+    for i in range(2, int(n**(1/2)+1)): #2以上√a以下(整数)で割り切れる値があるか調べる
+        if n % i == 0:
+            prime = False
+            break
+
+    if prime: #True
+        print("素数である")
+
+    else: #False
+        print("素数でない")          
